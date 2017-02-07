1e38a88aa8a41f540328001e18af7e964940b862
Martin Kierkegaard <mark@easj.dk>
2017-02-07 12:10:51 +0100

    Tilføjet nye bil objekter til Listen

--- RecapNedarvning/Program.cs
+++ RecapNedarvning/Program.cs
@@ -34,23 +34,23 @@ namespace RecapNedarvning
             //elBilListe.Add(new ElBil(200000, 2016, 16, 20));
 
             var bilListe = new List<Bil>();
-            //bilListe.Add(new Dieselbil(5000, 2014, 20, 30));
-            //bilListe.Add(new Dieselbil(15000, 2015, 28, 40));
-            //bilListe.Add(new Dieselbil(280000, 2015, 17, 50));
-            //bilListe.Add(new Dieselbil(35000, 2016, 12, 70, false));
-            //bilListe.Add(new Dieselbil(81400, 2012, 17, 60, false));
+            bilListe.Add(new Dieselbil(5000, 2014, "Ford","DB 22331", 20, 30));
+            bilListe.Add(new Dieselbil(15000, 2015,"Ford", "DB 24331", 28, 40));
+            bilListe.Add(new Dieselbil(280000, 2015, "Fiat", "DB 92331", 17, 50));
+            bilListe.Add(new Dieselbil(35000, 2016, "BMW", "DB 32111", 12, 70, false));
+            bilListe.Add(new Dieselbil(81400, 2012, "VW", "DB 77231", 17, 60, false));
 
-            //bilListe.Add(new BenzinBil(30000, 2016, 15, 40));
-            //bilListe.Add(new BenzinBil(150000, 2014, 17, 30));
-            //bilListe.Add(new BenzinBil(39000, 2015, 12, 80));
-            //bilListe.Add(new BenzinBil(400000, 2016, 11, 50));
-            //bilListe.Add(new BenzinBil(120000, 2016, 48, 25));
+            bilListe.Add(new BenzinBil(30000, 2016, "Fiat", "BB 21131", 15, 40));
+            bilListe.Add(new BenzinBil(150000, 2014, "Seat", "BB 23569", 17, 30));
+            bilListe.Add(new BenzinBil(39000, 2015, "Audi", "BB 21444", 12, 80));
+            bilListe.Add(new BenzinBil(400000, 2016, "Ford", "BB 23331", 11, 50));
+            bilListe.Add(new BenzinBil(120000, 2016, "BMW", "BB 19931", 48, 25));
 
-            //bilListe.Add(new ElBil(200000, 2015, 10, 50));
-            //bilListe.Add(new ElBil(200000, 2015, 13, 50));
-            //bilListe.Add(new ElBil(200000, 2015, 9, 30));
-            //bilListe.Add(new ElBil(200000, 2016, 12, 30));
-            //bilListe.Add(new ElBil(200000, 2016, 16, 20));
+            bilListe.Add(new ElBil(200000, 2015, "VW", "EB 11111", 10, 50));
+            bilListe.Add(new ElBil(200000, 2015, "Nissan", "ELBIL", 13, 50));
+            bilListe.Add(new ElBil(200000, 2015, "Tesla", "1", 9, 30));
+            bilListe.Add(new ElBil(200000, 2016, "Renault", "EB 2", 12, 30));
+            bilListe.Add(new ElBil(200000, 2016, "Ellert", "EB 31233", 16, 20));
 
 
             int tæller =0;
@@ -66,7 +66,7 @@ namespace RecapNedarvning
                     Console.WriteLine("Har partikelfilter: " + dBil.PartikelFilter);
                 }
 
-                if (bil is BenzinBil)
+                else if (bil is BenzinBil)
                 {
                     var bBil = bil as BenzinBil;
                     Console.WriteLine("Det er en benzinbil");
@@ -74,7 +74,7 @@ namespace RecapNedarvning
 
                 }
 
-                if (bil is ElBil)
+                else if (bil is ElBil)
                 {
                     var eBil = bil as ElBil;
                     Console.WriteLine("Det er en elbil");
